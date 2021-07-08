24f7036dc1d990dd02e641268d90910a5c7a745e
Lauan Borges Guermandi <lbguermandi@paschoalotto.com.br>
2021-07-08 01:18:01 -0300

    feat: adição da estrutura inicial do projeto

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,32 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        protected Categoria() { }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+
+            Validar();
+        }
+
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O Nome da categoria não pode estar vazio!");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O Código não pode ser 0!");
+        }
+
+    }
+}
