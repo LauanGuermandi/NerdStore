385505795b9290599324c529e15e2cc86faef617
Lauan Borges Guermandi <lbguermandi@paschoalotto.com.br>
2021-08-02 03:08:15 -0300

    feat: adição do contexto de vendas

---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,80 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido : Entity, IAggregateRoot
+    {
+        public int Codigo { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        // EF relations
+        public virtual Voucher Voucher { get; private set; }
+
+        public Pedido(Guid clienteId, bool voucherUtilizado, decimal desconto, decimal valorTotal)
+        {
+            ClienteId = clienteId;
+            VoucherUtilizado = voucherUtilizado;
+            Desconto = desconto;
+            ValorTotal = valorTotal;
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        protected Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        public void AplicarVoucher(Voucher voucher)
+        {
+            Voucher = voucher;
+            VoucherUtilizado = true;
+            CalcularValorPedido();
+        }
+
+        public void CalcularValorPedido()
+        {
+            ValorTotal = PedidoItems.Sum(p => p.CalcularValor());
+            CalcularValorTotalDesconto();
+        }
+
+        public void CalcularValorTotalDesconto()
+        {
+            if (!VoucherUtilizado) return;
+
+            decimal desconto = 0;
+            var valor = ValorTotal;
+
+            if (Voucher.TipoDescontoVoucher == TipoDescontoVoucher.Porcentagem)
+            {
+                if (Voucher.Percentual.HasValue)
+                {
+                    desconto = (valor * Voucher.Percentual.Value) / 100;
+                    valor -= desconto;
+                }
+            }
+            else
+            {
+                if (Voucher.ValorDesconto.HasValue)
+                {
+                    desconto = Voucher.ValorDesconto.Value;
+                    valor -= desconto;
+                }
+            }
+
+            ValorTotal = valor < 0 ? 0 : valor;
+            Desconto = desconto;
+        }
+    }
+}
